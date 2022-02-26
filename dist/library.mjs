c5c16c9fd007c06a16c7081fb169a64906938f66
Robert <treboryx@gmail.com>
2022-02-26 14:48:34 +0200

    bump

--- dist/library.mjs
+++ dist/library.mjs
@@ -62,32 +62,32 @@ var script = {
     vSrc() {
       switch (this.vPlatform) {
         case "microsoft":
-          return `https://cdn.jsdelivr.net/gh/emojes/assets@master/microsoft/${this.vSize}/${this.vImage}.png`;
+          return `https://cdn.jsdelivr.net/gh/emojes/assets@master/microsoft/png/${this.vSize}/${this.vImage}.png`;
         case "twitter":
         case "tw":
           switch (this.vFormat) {
             case "svg":
               return `https://cdn.jsdelivr.net/gh/twitter/twemoji@master/assets/svg/${this.vImage}.svg`;
             case "png":
-              return `https://cdn.jsdelivr.net/gh/emojes/assets@master/twitter/${this.vSize}/${this.vImage}.png`;
+              return `https://cdn.jsdelivr.net/gh/emojes/assets@master/twitter/png/${this.vSize}/${this.vImage}.png`;
           }
         case "ios":
         case "apple":
-          return `https://cdn.jsdelivr.net/gh/emojes/assets@master/apple/${this.vSize}/${this.vImage}.png`;
+          return `https://cdn.jsdelivr.net/gh/emojes/assets@master/apple/png/${this.vSize}/${this.vImage}.png`;
         case "google":
         case "noto":
           switch (this.vFormat) {
             case "svg":
               return `https://cdn.jsdelivr.net/gh/googlefonts/noto-emoji@master/svg/emoji_u${this.vImage}.svg`;
             case "png":
-              return `https://cdn.jsdelivr.net/gh/emojes/assets@master/google/${this.vSize}/emoji_u${this.vImage}.png`;
+              return `https://cdn.jsdelivr.net/gh/googlefonts/noto-emoji@master/png/${this.vSize}/emoji_u${this.vImage}.png`;
           }
         case "facebook":
         case "fb":
-          return `https://cdn.jsdelivr.net/gh/emojes/assets@master/facebook/${this.vSize}/${this.vImage}.png`;
+          return `https://cdn.jsdelivr.net/gh/emojes/assets@master/facebook/png/${this.vSize}/${this.vImage}.png`;
         case "joy":
         case "joypixels":
-          return `https://cdn.jsdelivr.net/gh/emojes/assets@master/joypixels/${this.vSize}/${this.vImage}.png`;
+          return `https://cdn.jsdelivr.net/gh/emojes/assets@master/joypixels/png/${this.vSize}/${this.vImage}.png`;
         default:
           return `https://cdn.jsdelivr.net/gh/twitter/twemoji@master/assets/svg/${this.vImage}.svg`;
       }
@@ -101,14 +101,14 @@ var script = {
     vFallback() {
       switch (this.vFormat) {
         case "svg":
-          return `https://cdn.jsdelivr.net/gh/twitter/twemoji@master/assets/svg/${toCodePoints(this.emoji, true).join(
-            "-"
-          )}.svg`;
-        case "png":
-          return `https://cdn.jsdelivr.net/gh/emojes/assets@master/twitter/${this.vSize}/${toCodePoints(
+          return `https://cdn.jsdelivr.net/gh/twitter/twemoji@master/assets/svg/${toCodePoints(
             this.emoji,
             true
-          ).join("-")}.png`;
+          ).join("-")}.svg`;
+        case "png":
+          return `https://cdn.jsdelivr.net/gh/emojes/assets@master/twitter/png/${
+            this.vSize
+          }/${toCodePoints(this.emoji, true).join("-")}.png`;
       }
     },
     vSize() {
@@ -140,7 +140,8 @@ var script = {
         e.target.src = img.src;
       };
       img.onerror = () => {
-        e.target.src = "https://cdn.jsdelivr.net/gh/twitter/twemoji@master/assets/svg/2754.svg";
+        e.target.src =
+          "https://cdn.jsdelivr.net/gh/twitter/twemoji@master/assets/svg/2754.svg";
       };
     },
   },
